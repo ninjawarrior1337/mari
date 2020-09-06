0450f9e36d86cacbf57e1b0f1870a7ed19d3402e
ninjawarrior1337 <teelar1337@gmail.com>
2020-09-05 22:55:20 -0700

    add logo and update packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import App, { AppProps } from "next/app";
 import Head from "next/head"
 import FileStore from "../store/FileStore";
 import React from "react";
-import "mobx-react/batchingForReactDom";
+// import "mobx-react/batchingForReactDom";
 import { autorun, observable } from "mobx";
 
 export const FileStoreContext: React.Context<FileStore> = React.createContext(
@@ -29,8 +29,10 @@ export default class MyApp extends App<AppProps, {}, {}> {
             <>
                 <Head>
                     <title>Mari - A Peer to Peer Filesharing Service</title>
+                    <link rel="icon" href="/mari.png"></link>
                     <meta name="description" content="A Peer to Peer Filesharing service powered by WebTorrent"></meta>
                     <meta property="og:title" content="Mari" />
+                    <meta property="og:image" content="https://mari.treelar.cf/mari.png" />
                     <meta property="og:description" content="A Peer to Peer Filesharing service" />
                     <meta property="og:type" content="website"/>
                 </Head>
